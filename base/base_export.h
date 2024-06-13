debf57c7dcbcaf5ca4f9e8838d661002253abcaf
zhongtianzhou <zhongtianzhou@360.cn>
2024-06-13 17:09:08 +0800

    use cmake

--- base/base_export.h
+++ base/base_export.h
@@ -5,12 +5,8 @@
 #ifndef BASE_BASE_EXPORT_H_
 #define BASE_BASE_EXPORT_H_
 
-#ifndef COMPONENT_BUILD
-#define COMPONENT_BUILD
-#endif
-
 #if defined(COMPONENT_BUILD)
-#if defined(WIN32) || defined(_MSC_VER)
+#if defined(WIN32)
 
 #if defined(BASE_IMPLEMENTATION)
 #define BASE_EXPORT __declspec(dllexport)
